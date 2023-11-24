5db77d6be63bee3c71d8588d98e20ea07fac6f38
Simon K <jackofspaces@gmail.com>
2023-11-24 10:38:24 +0000

    `44`: Basic `panic`

--- panics/main.go
+++ panics/main.go
@@ -1,5 +1,16 @@
 package panics
 
+// Exceptions in golang are propagated in the form of panics.
+// Typically when something unexpected occurs and we want to
+// exit in a fail-fast manner, panic is called.
 func Run() {
+	somethingWrong(100)
+	somethingWrong(4)
 
+}
+
+func somethingWrong(n int) {
+	if n < 10 {
+		panic("n was less than 10")
+	}
 }
