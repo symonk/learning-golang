a5c998947d399e0c72c020385c1416a6452cfd29
Simon K <jackofspaces@gmail.com>
2023-11-25 10:41:54 +0000

    `79`: Executing commands using `syscall` & `exec`

--- execingprocesses/main.go
+++ execingprocesses/main.go
@@ -1,6 +1,29 @@
 package execingprocesses
 
-// Similarly to spawning a subprocess
+import (
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+// Similarly to spawning a subprocess, exec
+// can replace the go process with another one
+// As always, taking user input into exec directly
+// is a serious security flaw and should be used with caution.
 func Run() {
+	listFilesAndDirectories()
+}
+
+func listFilesAndDirectories() {
+	executable, err := exec.LookPath("ls")
+	if err != nil {
+		// POSIX only here again.
+		panic(err)
+	}
+	args := []string{"ls", "-a", "-l", "-h"}
+	env := os.Environ()
+	if err := syscall.Exec(executable, args, env); err != nil {
+		panic(err)
+	}
 
 }
