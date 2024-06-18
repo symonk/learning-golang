f200bdf65efa1a61c26e56fbef26138b88edc406
Simon K <jackofspaces@gmail.com>
2024-06-18 19:13:37 +0100

    adding an example of concurrency with `error` handling propagation

---
+++ collectingerrors/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"sync"
+	"time"
+)
+
+var sentinelErr = errors.New("timed out")
+
+// Often it is desirable to collect the output of various goroutines
+// into one central error.  We can achieve this by using a waitgroup
+// notifications channel and errors.Join
+func main() {
+	if err := run(); err != nil {
+		fmt.Printf("We got an error %s", err.Error())
+		if errors.Is(err, sentinelErr) {
+			fmt.Println("Sentinel error.")
+		}
+	}
+}
+
+func run() error {
+	var wg sync.WaitGroup
+	size := 10
+	results := make(chan error, size)
+	wg.Add(size)
+
+	// spawn 10 goros; shoving their error results into the channel
+	for i := 0; i < size; i++ {
+		go func(i int) {
+			defer wg.Done()
+			time.Sleep(time.Second)
+			results <- sentinelErr
+		}(i)
+	}
+
+	wg.Wait()
+	close(results)
+	combinedErrors := make([]error, size)
+	for error := range results {
+		combinedErrors = append(combinedErrors, error)
+	}
+	return errors.Join(combinedErrors...)
+}
