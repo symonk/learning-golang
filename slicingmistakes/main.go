d41e7b6a356415c0fc9398e682b5875cc22353bb
Simon K <jackofspaces@gmail.com>
2024-06-30 14:58:02 +0100

    remove bogus code in `slicingmistakes`

---
+++ slicingmistakes/main.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+	/*
+		Slices in go are essentially a ptr to a backed array and a capacity
+		and length.  There are a number of edge case caveats when dealing with
+		slices, these are outlined throughout this module.
+	*/
+}
